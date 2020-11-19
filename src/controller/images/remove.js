f8a7c56a424dade12a2e416d178851107a6d9108
luzhongkuan <luzhongk@126.com>
2020-11-20 00:30:02 +0800

    fix: 删除文件

--- src/controller/images/remove.js
+++ src/controller/images/remove.js
@@ -1,5 +1,18 @@
-function remove(ctx) {
-  ctx.state.data = '删除成功'
+const path = require('path')
+const fs = require('fs-extra')
+const config = require('../../config')
+
+async function remove(ctx) {
+  const { names } = ctx.request.query
+  const namesArr = names.split(',')
+  for (let i = 0; i < namesArr.length; i++) {
+    const filePath = path.resolve(config.imagesPath, namesArr[i])
+    const isExist = await fs.pathExists(filePath)
+    if (isExist) {
+      await fs.remove(filePath)
+    }
+  }
+  ctx.state.data = namesArr
 }
 
 module.exports = remove
