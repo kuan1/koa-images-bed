5dcddfb1ae39ff0f8500bc011fcdfe7460711955
luzhongkuan <luzhongk@126.com>
2020-11-19 23:47:41 +0800

    feat: 上传文件

--- src/controller/images/upload.js
+++ src/controller/images/upload.js
@@ -1,4 +1,19 @@
-function upload(ctx) {
+const fs = require('fs-extra')
+const path = require('path')
+const config = require('../../config')
+
+async function save(file) {
+  const newName = file.hash + file.name.substr(file.name.lastIndexOf('.'))
+  await fs.rename(file.path, path.resolve(config.imagesPath, newName))
+}
+
+async function upload(ctx) {
+  const { file } = ctx.request.files
+  if (Array.isArray(file)) {
+    file.forEach((item) => save(item))
+  } else {
+    save(file)
+  }
   ctx.state.data = '上传成功'
 }
 
--- src/routers/images.js
+++ src/routers/images.js
@@ -1,9 +1,25 @@
 const router = require('koa-router')()
+const koaBody = require('koa-body')
 const controller = require('../controller')
+const config = require('../config')
 
-//
+const FOUR_M = 20 * 1024 * 1024
+
 router.get('/upload', controller.images.list)
-router.post('/upload', controller.images.upload)
+
+router.post(
+  '/upload',
+  koaBody({
+    multipart: true,
+    formidable: {
+      maxFieldsSize: FOUR_M,
+      uploadDir: config.imagesPath,
+      keepExtensions: true,
+      hash: 'md5',
+    },
+  }),
+  controller.images.upload
+)
 router.delete('/upload', controller.images.remove)
 
 module.exports = router
