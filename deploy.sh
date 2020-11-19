6b424858eb9cc0f2ab8394617f950759c11bd7eb
luzhongkuan <luzhongk@126.com>
2020-11-20 00:34:57 +0800

    fix: 上传服务器

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+
+# 上传到个人服务器
+scp -r src static package.json pm2.json tencent:/opt/koa-images-bed
--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const isDev = process.env.NODE_ENV === 'development'
 
 module.exports = {
-  port: '9301',
+  port: process.env.PORT || '9301',
   imagesPath: path.resolve(__dirname, '../static/__images'),
   maxAmount: isDev ? 10 : 1000,
 }
