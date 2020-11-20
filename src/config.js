10a6d0d116d61bf11c2c974d8c3b45c04c312d7b
luzhongkuan <luzhongk@126.com>
2020-11-20 11:49:39 +0800

    fix: bug

--- src/config.js
+++ src/config.js
@@ -5,4 +5,5 @@ module.exports = {
   port: process.env.PORT || '9301',
   imagesPath: isDev ? path.resolve(__dirname, '../__images') : '/var/www/static/images',
   maxAmount: isDev ? 10 : 1000,
+  prefix: 'https://www.luzhongkuan.cn/static/images/',
 }
--- src/utils/util.js
+++ src/utils/util.js
@@ -1,15 +1,5 @@
-const fs = require('fs-extra')
-const cache = require("./cache")
-const { imagesPath } = require('../config')
+const { prefix } = require('../config')
 
-export function getImages() {
-  const cacheImages = cache.get() || []
-  if (cacheImages.length) return cacheImages
-  await fs.ensureDir(imagesPath)
-  const res = await fs.readdir(imagesPath)
-  return res
-}
-
-export function formatImages(list) {
-  return list.map((name) => `https://www.luzhongkuan.cn/static/images/${name}`)
+exports.formatImages = function formatImages(list) {
+  return list.map((name) => `${prefix}${name}`)
 }
